03101ddc22612a0e0e0ef335fff33346c6bd8787
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2024-10-03 14:24:55 +0500

    MongoDB Connection

--- Backend/App.js
+++ Backend/App.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const MongoConnect = require('./util/database');
 
 const app = express();
 
 app.use(cors());
 
-app.get("/getData", (req, res, next)=>{
-    res.send("Hello from Backend");
+MongoConnect(client =>{
+    app.listen(3000);
+    console.log(client);
 });
 
-app.listen(3000);
-console.log("App is Running");
+// app.get("/getData", (req, res, next)=>{
+//     res.send("Hello from Backend");
+// });
+
+// app.listen(3000);
+// console.log("App is Running");
---
+++ Backend/util/database.js
@@ -0,0 +1,15 @@
+const MongoDB = require("mongodb");
+const MongoClient = MongoDB.MongoClient;
+
+const MongoConnect = callback =>{
+    MongoClient.connect('mongodb+srv://myzameen:Pakistan101010.@cluster0.vsfpl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+        .then(client =>{
+        console.log("Connected to MongoDB");
+        callback(client);
+        })
+        .catch(err =>{
+        console.log(err);
+        })
+}
+
+module.exports = MongoConnect;
