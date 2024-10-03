23366b748d8d6252ba7b3e09e53f4cc7d7f4d703
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2024-10-03 17:20:34 +0500

    Signup System

--- Backend/App.js
+++ Backend/App.js
@@ -2,19 +2,19 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const MongoConnect = require('./util/database');
+const bodyParser = require('body-parser');
+
+const authRoutes = require('./Routes/authRoutes');
 
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 MongoConnect(client =>{
+    app.locals.db = client.db();
+    app.use('/api', authRoutes);
     app.listen(3000);
     console.log(client);
 });
-
-// app.get("/getData", (req, res, next)=>{
-//     res.send("Hello from Backend");
-// });
-
-// app.listen(3000);
-// console.log("App is Running");
