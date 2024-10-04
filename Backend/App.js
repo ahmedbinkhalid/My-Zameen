d241024f1d74fc5006353a54396411d08e61beda
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2024-10-04 15:41:16 +0500

    Updated Cors

--- Backend/App.js
+++ Backend/App.js
@@ -12,6 +12,13 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use((req, res, next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 MongoConnect(client =>{
     app.locals.db = client.db('myzameen');
     app.use('/api', authRoutes);
