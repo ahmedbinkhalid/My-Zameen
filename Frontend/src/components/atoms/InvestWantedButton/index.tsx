7226ace0a6515743aa02333d1e3be1f01122ce10
Hassan Javed <hassan.javed.softwareengineer@gmail.com>
2024-10-05 17:45:58 +0500

    hgh

--- Frontend/src/components/atoms/InvestWantedButton/index.tsx
+++ Frontend/src/components/atoms/InvestWantedButton/index.tsx
@@ -8,7 +8,7 @@ export const InvestWantedButton: React.FC<BtnData> = ({ data }) => {
   return (
     <button
       className={`rounded-lg py-3 text-white bg-[#37474F] uppercase text-sm font-sans font-bold self-center w-32 ${! (data === "Login" || "CREATE Account") ? "mt-8" : "h-full w-full"}`}
-    >
+    >//ghghghghghy
       {data}
     </button>
   );
