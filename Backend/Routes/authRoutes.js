490e6bade38998152a90930d14997cf6858dc853
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2024-10-05 15:01:24 +0500

    Added the express rate limiter

--- Backend/Routes/authRoutes.js
+++ Backend/Routes/authRoutes.js
@@ -2,6 +2,18 @@ const express = require('express');
 const { signUp } = require('../Controllers/authConrtoller');
 const { login, resetpassword, forgotpassword } = require('../Controllers/authConrtoller')
 const router = express.Router();
+const rateLimit = require('express-rate-limit');
+
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 100,
+    max: 5,
+    message: {
+        status: 429,
+        message: 'To many requests, please try again later.'
+    }
+});
+
+router.use(limiter);
 
 router.post('/signup', signUp);
 router.post('/login', login);
--- Backend/Routes/blogRoutes.js
+++ Backend/Routes/blogRoutes.js
@@ -3,11 +3,24 @@ const { submitBlog, approveBlog, getpendingBlog, rejectBlog } = require('../Cont
 const { isBlogger, isAdmin, verifyToken } = require('../Middlewares/middleware');
 const express = require('express');
 const router = express.Router();
+const ratelimit = require('express-rate-limit');
+
+// Defining the rate limit for all the routes in this router
+const limiter = ratelimit({
+    windowMs: 15 * 60 * 100,
+    max: 100,
+    message: {
+        status: 429,
+        message: 'To many requests, please try again later.'
+    }
+});
+// Applying the rate limit on all the routes in this router
+router.use(limiter);
 
 router.post('/submit', verifyToken, isBlogger , submitBlog);
 router.post('/approve', verifyToken, isAdmin,approveBlog);
 router.get('/pending', verifyToken, isAdmin ,getpendingBlog);
-router.get('/reject', verifyToken, isAdmin, rejectBlog);
+router.post('/reject', verifyToken, isAdmin, rejectBlog);
 
 module.exports = router;
 
